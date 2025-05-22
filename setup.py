a3b8b71f6f59ab63b88d558ff3d89273058a1bf1
yuzoo0226 <fryuzoo@gmail.com>
2025-05-22 23:25:34 +0900

    update python packages name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="segment_anything",
+    name="segment_anything_langsplat",
     version="1.0",
     install_requires=[],
     packages=find_packages(exclude="notebooks"),
